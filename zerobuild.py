0a9efe58bd1672325f5ce6334ede8fb759e3643b
haseeb <haseeeb8j@gmail.com>
2026-07-24 18:04:00 +0500

    updated: zerobuild make file and added a single cmake file

--- zerobuild.py
+++ zerobuild.py
@@ -2,42 +2,50 @@ from zero import *
 
 build = Build()
 build.compiler = "gcc"
-build.directory = "build"
+build.directory = ".build"
 
-src = Path("Engine")
+# Cute Engine
 
 CuteEngine = StaticLibrary()
+
+cute_engine_src = Path("Engine")
+
 CuteEngine.source = Source(
-	src / "engine" / "engine.c",
-	src / "engine" / "exec.c",
-	src / "engine" / "context.c",
-	src / "object" / "object.c",
-	src / "container" / "container.c",
-	src / "image" / "image.c",
-	src / "utils" / "utils.c",
+	cute_engine_src / "engine" / "engine.c",
+	cute_engine_src / "engine" / "exec.c",
+	cute_engine_src / "engine" / "context.c",
+	cute_engine_src / "object" / "object.c",
+	cute_engine_src / "container" / "container.c",
+	cute_engine_src / "image" / "image.c",
+	cute_engine_src / "utils" / "utils.c",
 )
-CuteEngine.headers.public = src, src / "include"
-CuteEngine.arguments = "-w", "-fpermissive"
+
+CuteEngine.headers.public = cute_engine_src / "include"
+CuteEngine.headers.private = cute_engine_src
+CuteEngine.arguments = "-Wall",  "-Wextra",  "-g"
+
+# Engine Entry Point
 
 cute = Executable()
-cute.source = Source(
-	"main/main.c"
-)
-cute.arguments = "-w", "-fpermissive"
+cute.source = Source("main/main.c")
+cute.arguments = "-Wall",  "-Wextra",  "-g"
 cute.link(CuteEngine)
 
+# Assembler
 
-src = Path("Assembler")
 cuteasm = Executable()
-cuteasm.name = "cute-asm"
 cuteasm.compiler = "g++"
+
+cute_asm_src = Path("Assembler")
+
 cuteasm.source = Source(
-	src / "main.cpp",
-	src / "tokenizer/tokenizer.cpp",
-	src / "tokenizer/stream.cpp",
-	src / "parser/parser.cpp",
-	src / "codegen/codegen.cpp"
+	cute_asm_src / "main.cpp",
+	cute_asm_src / "tokenizer/tokenizer.cpp",
+	cute_asm_src / "tokenizer/stream.cpp",
+	cute_asm_src / "parser/parser.cpp",
+	cute_asm_src / "codegen/codegen.cpp"
 )
-cuteasm.headers.private = src
-cuteasm.arguments = "-w", "-fpermissive"
+
+cuteasm.headers.private = cute_asm_src
+cuteasm.arguments = "-Wall",  "-Wextra",  "-g"
 cuteasm.link(CuteEngine)
