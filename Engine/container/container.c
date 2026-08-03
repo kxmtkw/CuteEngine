5d316663e47f7babd4e4b6cbc589fa3f213aa6fe
haseeb <haseeeb8j@gmail.com>
2026-08-03 22:30:25 +0500

    updated: container method arguments and refactored code

--- Engine/container/container.c
+++ Engine/container/container.c
@@ -15,12 +15,13 @@
 
 
 
-CtObject*
-ct_container_new(CtObjectManager* manager, uint32_t size,  ctError* err) {
+CtContainer*
+ct_container_new(CtObjectManager* manager, uint32_t size, ctError* err) {
 
-	uint32_t req_size = sizeof(ctContainer) + sizeof(CtAtom) * size + sizeof(CtAtomTypeSize) * size;
+	uint32_t req_size = sizeof(CtContainer) + sizeof(CtAtom) * size + sizeof(CtAtomTypeSize) * size;
 
 	CtObject* obj = ct_objects_new_object(manager, req_size, 0 /*placeholder*/, ct_container_del);
+	CtContainer* container = (CtContainer*) obj;
 
 	if (ct_objects_check_error(manager)) {
 		*err = manager->error;
@@ -28,26 +29,25 @@ ct_container_new(CtObjectManager* manager, uint32_t size,  ctError* err) {
 	};
 	
 	uint8_t* data = (uint8_t*) obj;
-	ctContainer* container = (ctContainer*) obj;
-	container->atoms = (CtAtom*) (data + sizeof(ctContainer));
-	container->types = (CtAtomTypeSize*) (data + sizeof(ctContainer) + sizeof(CtAtom) * size);
+	container->atoms = (CtAtom*) (data + sizeof(CtContainer));
+	container->types = (CtAtomTypeSize*) (data + sizeof(CtContainer) + sizeof(CtAtom) * size);
 	container->size = size;
 	container->sub_objects = 0;
 
 	CT_LOG("containers", "New container (%u.%u) [%p] (size:%u atoms) allocated.\n", obj->bucket_id, obj->bucket_index, obj, size);
 
-	return obj;
+	return container;
 };
 
 
 void
 ct_container_del(CtObjectManager* manager, CtObject* obj) {
 
-	ctContainer* container = (ctContainer*) obj;
+	CtContainer* container = (CtContainer*) obj;
 
 	uint32_t j = 0;
 	
-	for (uint32_t i = 0; i < container->size && j < container->sub_objects; i++) {
+	for (uint32_t i = 0; i < obj->size && j < container->sub_objects; i++) {
 		if (container->types[i] == CT_ATOM_OBJECT) {
 			ct_objects_dec_ref(manager, container->atoms[i].as_object);
 			j++;
@@ -60,74 +60,66 @@ ct_container_del(CtObjectManager* manager, CtObject* obj) {
 
 // Get an atom in the container. Will return ctConManagerCode_OutOfBounds if index is out of bounds
 CtTypedAtom
-ct_container_get(CtObjectManager* manager, CtObject* obj, uint32_t index, ctError* err) {
+ct_container_get(CtObjectManager* manager, CtContainer* obj, uint32_t index, ctError* err) {
 
-	ctContainer* container = (ctContainer*) obj;
-
-	if (index >= container->size) {
-		CT_ERROR(err, ctErrorCode_OutOfBounds, "Can not access container slot #%u (>= %u)", index, container->size);
+	if (index >= obj->size) {
+		CT_ERROR(err, ctErrorCode_OutOfBounds, "Can not access container slot #%u (>= %u)", index, obj->size);
 		return (CtTypedAtom){CT_ATOM_PRIMITIVE, (CtAtom){0}};
 	}
 
-	return (CtTypedAtom){container->types[index], container->atoms[index]};
+	return (CtTypedAtom){obj->types[index], obj->atoms[index]};
 };
 
 // Set an atom in the container. Will return ctConManagerCode_OutOfBounds if index is out of bounds.
 void
-ct_container_set(CtObjectManager* manager, CtObject* obj, uint32_t index, CtTypedAtom atom, ctError* err) {
+ct_container_set(CtObjectManager* manager, CtContainer* obj, uint32_t index, CtTypedAtom atom, ctError* err) {
 
-	ctContainer* container = (ctContainer*) obj;
-
-	if (index >= container->size) {
-		CT_ERROR(err, ctErrorCode_OutOfBounds, "Can not access container slot #%u (>= %u)", index, container->size);
+	if (index >= obj->size) {
+		CT_ERROR(err, ctErrorCode_OutOfBounds, "Can not access container slot #%u (>= %u)", index, obj->size);
 		return;
 	}
 
-	if (container->types[index] == CT_ATOM_OBJECT) {
-		ct_objects_dec_ref(manager, container->atoms[index].as_object);
-		if (container->sub_objects > 0) container->sub_objects--;
+	if (obj->types[index] == CT_ATOM_OBJECT) {
+		ct_objects_dec_ref(manager, obj->atoms[index].as_object);
+		if (obj->sub_objects > 0) obj->sub_objects--;
 	}
 
-	container->atoms[index] = atom.atom;
-	container->types[index] = atom.type;
+	obj->atoms[index] = atom.atom;
+	obj->types[index] = atom.type;
 
-	if (container->types[index] == CT_ATOM_OBJECT) {
-		ct_objects_inc_ref(manager, container->atoms[index].as_object);
-		container->sub_objects++; 
+	if (obj->types[index] == CT_ATOM_OBJECT) {
+		ct_objects_inc_ref(manager, obj->atoms[index].as_object);
+		obj->sub_objects++; 
 	}
 }
 
 // Create a shallow copy of a container
-CtObject*
-ct_container_copy(CtObjectManager* manager, CtObject* src, ctError* err) {
+CtContainer*
+ct_container_copy(CtObjectManager* manager, CtContainer* obj, ctError* err) {
 	
-	ctContainer* consrc = (ctContainer*) src;
-
-	CtObject* copy = ct_container_new(manager, consrc->__object__.obj_size, err);
+	CtContainer* copy = ct_container_new(manager, obj->__object__.size, err);
 
 	if (err->code) {return NULL;}
-
-	ctContainer* concopy = (ctContainer*) copy;
 
 	if (!copy) {
 		return NULL;
 	}
 
-	memcpy(concopy->atoms, consrc->atoms, consrc->size * sizeof(CtAtom));
-	memcpy(concopy->types, consrc->types, consrc->size * sizeof(CtAtomTypeSize));
+	memcpy(copy->atoms, obj->atoms, obj->size * sizeof(CtAtom));
+	memcpy(copy->types, obj->types, obj->size * sizeof(CtAtomTypeSize));
 
-	concopy->sub_objects = consrc->sub_objects;
+	copy->sub_objects = obj->sub_objects;
 
 	uint32_t j = 0;
 
-	for (uint32_t i = 0; i < consrc->size && j < consrc->sub_objects; i++) {
-		if (consrc->types[i] == CT_ATOM_OBJECT) {
-			ct_objects_inc_ref(manager, consrc->atoms[i].as_object);
+	for (uint32_t i = 0; i < obj->size && j < obj->sub_objects; i++) {
+		if (obj->types[i] == CT_ATOM_OBJECT) {
+			ct_objects_inc_ref(manager, obj->atoms[i].as_object);
 			j++;
 		}
 	}
 	
-	CT_LOG("containers", "Copied container (%u.%u) [%p] from container (%u.%u) [%p]\n", copy->bucket_id, copy->bucket_index, copy, src->bucket_id, src->bucket_index, src);
+	CT_LOG("containers", "Copied container (%u.%u) [%p] from container (%u.%u) [%p]\n", copy->__object__.bucket_id, copy->__object__.bucket_index, copy, obj->__object__.bucket_id, obj->__object__.bucket_index, obj);
 
 	return copy;
 }
--- Engine/container/container.h
+++ Engine/container/container.h
@@ -16,38 +16,33 @@ typedef struct {
 	uint32_t            size;
 	CtAtom*             atoms;
 	CtAtomTypeSize*     types;
-} ctContainer;
+} CtContainer;
 
 
-CtObject*
+CtContainer*
 ct_container_new(CtObjectManager* manager, uint32_t size, ctError* err);
 
+
 void
-ct_container_del(CtObjectManager* manager, CtObject* con);
+ct_container_del(CtObjectManager* manager, CtObject* obj);
 
 
 static inline uint32_t
-ct_container_size(CtObjectManager* manager, CtObject* con) {
-	ctContainer* container = (ctContainer*) con;
-	return container->size;
+ct_container_size(CtObjectManager* manager, CtContainer* obj) {
+	return obj->size;
 };
 
 
 // Get an atom in the container. Will return ctConManagerCode_OutOfBounds if index is out of bounds
 CtTypedAtom
-ct_container_get(CtObjectManager* manager, CtObject* container, uint32_t index, ctError* err);
+ct_container_get(CtObjectManager* manager, CtContainer* obj, uint32_t index, ctError* err);
 
 // Set an atom in the container. Will return ctConManagerCode_OutOfBounds if index is out of bounds.
 void
-ct_container_set(CtObjectManager* manager, CtObject* container, uint32_t index, CtTypedAtom atom, ctError* err);
+ct_container_set(CtObjectManager* manager, CtContainer* obj, uint32_t index, CtTypedAtom atom, ctError* err);
 
 // Create a shallow copy of a container
-CtObject*
-ct_container_copy(CtObjectManager* manager, CtObject* src, ctError* err);
-
-// Create a deep copy of a container
-CtObject*
-ct_container_deepcopy(CtObjectManager* manager, CtObject* src, ctError* err);
-
+CtContainer*
+ct_container_copy(CtObjectManager* manager, CtContainer* obj, ctError* err);
 
 #endif // CONTAINERS_CONTAINER_H
