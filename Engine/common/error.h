a0149cb23b07e658144ea313b2d1be73cf30e491
haseeb <haseeeb8j@gmail.com>
2026-08-04 20:22:50 +0500

    updated: error impl, moving to thread errors

--- Engine/common/error.h
+++ Engine/common/error.h
@@ -1,65 +1,39 @@
 
-#ifndef ENGINE_ERROR_H
-#define ENGINE_ERROR_H
+#ifndef CT_ERROR_H
+#define CT_ERROR_H
 
+#include "utils/utils.h"
 #include <stdint.h>
 #include <stdio.h>
 
-
 typedef enum {
-
-    ctErrorCode_None            = 0x00,
-    ctErrorCode_Engine          = 0x01,
-    ctErrorCode_Procedure       = 0x02,
-    ctErrorCode_Module          = 0x03,
-	ctErrorCode_OutOfBounds     = 0x04,
-
-    ctErrorCode_Type            = 0x10,
-    ctErrorCode_Value           = 0x11,
-    ctErrorCode_ZeroDivision    = 0x12,
-    ctErrorCode_Overflow        = 0x13,
-    ctErrorCode_Underflow       = 0x14,
-    ctErrorCode_Index           = 0x15,
-    ctErrorCode_Recursion       = 0x16,
-
-} ctErrorCode;
-
-static const char* ct_error_stringforms[] = {
-
-    [ctErrorCode_None]            = "NoError",
-    [ctErrorCode_Engine]          = "EngineError",
-    [ctErrorCode_Procedure]       = "ProcedureError",
-    [ctErrorCode_Module]          = "ModuleError",
-	[ctErrorCode_OutOfBounds]     = "OutOfBounds",
-
-    [ctErrorCode_Type]            = "TypeError",
-    [ctErrorCode_Value]           = "ValueError",
-    [ctErrorCode_ZeroDivision]    = "ZeroDivisionError",
-    [ctErrorCode_Overflow]        = "OverflowError",
-    [ctErrorCode_Underflow]       = "UnderflowError",
-    [ctErrorCode_Index]           = "IndexError",
-    [ctErrorCode_Recursion]       = "RecursionError"
-};
-
+	CT_ERROR_LEVEL_ENGINE = 0x00,
+	CT_ERROR_LEVEL_LIB = 0x01,
+	CT_ERROR_LEVEL_USER = 0x02,
+} CtErrorLevel;
 
 typedef struct {
-	ctErrorCode code;
-	char        msg[256];
-} ctError;
+	bool          raised;
+	CtErrorLevel  level;
+	char*         where;
+	char*         what;
+	char          details[256];
+} CtError;
 
+
+#define CT_ERROR(ERROR, LEVEL, WHERE, WHAT, DETAILS, ...) \
+ERROR.where = WHERE; ERROR.what = WHAT; \
+ct_utils_format(ERROR.details, sizeof(ERROR.details), DETAILS, __VA_ARGS__); \
+ERROR.raised = true; \
+ERROR.level = LEVEL;
+
+#define CT_ERROR_ENGINE(ERROR, WHERE, WHAT, DETAILS, ...) \
+CT_ERROR(ERROR, CT_ERROR_LEVEL_ENGINE, WHERE, WHAT, DETAILS, __VA_ARGS__)
 
 static inline void
-ct_error_print(ctError err) {
-	printf("[%s] %s\n", ct_error_stringforms[err.code], err.msg);
+ct_error_print(CtError* err) {
+	printf("%s.%s :: %s\n", err->where, err->what, err->details);
 }
 
-#define CT_ERROR(ERR, CODE, MSG, ...) \
-ERR->code = CODE; \
-ct_utils_format( \
-	ERR->msg,  \
-	sizeof(ERR->msg), \
-	MSG, __VA_ARGS__ \
-); \
 
-
-#endif // ENGINE_ERROR_H
+#endif // CT_ERROR_H
