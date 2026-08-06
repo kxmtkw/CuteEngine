33cea98eb749332994a00aa93d65d372d04fad15
haseeb <haseeeb8j@gmail.com>
2026-08-06 22:40:36 +0500

    added: modcall helper macros; updated: completed buffer module

--- Engine/modules/buffer.h
+++ Engine/modules/buffer.h
@@ -1,160 +1,245 @@
 #ifndef CT_MODULE_BUFFER_H
 #define CT_MODULE_BUFFER_H
 
+#include <stdint.h>
+
 #include "common/atom.h"
+#include "common/error.h"
 #include "engine/context.h"
 #include "modules/modulespec.h"
 
+
+CT_MODULE_NAME_DEFINE(buffer, "Buffer");
+
 #include "lib/buffer.h"
 
+// 0: new
 static void
 ct_module_buffer_new(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
-	uint32_t size = args.argument_atoms[0].as_uint;
+    CT_MODCALL_PRIMIT_ARG(0, uint32_t, size, as_uint);
 
-	CtBufferObject* buffer;
-
-	result->success = buffer = ct_lib_buffer_new(ct_ctx_get_object_manager(args.context), size);
-	
-	result->returned_atom.as_object = (CtObject*) buffer;
-	result->returned_type = CT_ATOM_OBJECT;
+    CtBufferObject* buffer = ct_lib_buffer_new(ct_ctx_get_object_manager(args.context), size);
+    
+    CT_MODCALL_RESULT(as_object, (CtObject*)buffer, CT_ATOM_OBJECT);
 }
 
 
+// 1: size
+static void
+ct_module_buffer_size(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    
+    CT_MODCALL_RESULT(as_uint, buffer->size, CT_ATOM_PRIMITIVE);
+}
+
+
+// 2: capacity
+static void
+ct_module_buffer_capacity(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    
+    CT_MODCALL_RESULT(as_uint, buffer->capacity, CT_ATOM_PRIMITIVE);
+}
+
+// 3: resize
 static void
 ct_module_buffer_resize(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint32_t size = args.argument_atoms[1].as_uint;
 
-	result->success = ct_lib_buffer_resize(buffer, size);
-
-	result->returned_atom.as_uint = buffer->size;
-	result->returned_type = CT_ATOM_PRIMITIVE;
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, new_size, as_uint);
+    
+    ct_lib_buffer_resize(buffer, new_size);
 }
 
+// 4: reserve
+static void
+ct_module_buffer_reserve(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, new_cap, as_uint);
+
+    ct_lib_buffer_reserve(buffer, new_cap);
+}
+
+// 5: truncate
+static void
+ct_module_buffer_truncate(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+
+    ct_lib_buffer_truncate(buffer);
+}
+
+// 6: get
+static void
+ct_module_buffer_get(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, index, as_uint);
+
+    uint8_t byte_val = 0;
+    if (ct_lib_buffer_get_byte(buffer, index, &byte_val)) {
+        CT_MODCALL_RESULT(as_uint, (uint32_t)byte_val, CT_ATOM_PRIMITIVE);
+    }
+}
+
+// 7: set
+static void
+ct_module_buffer_set(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, index, as_uint);
+    CT_MODCALL_PRIMIT_ARG(2, uint32_t, byte_val, as_uint);
+
+    ct_lib_buffer_set_byte(buffer, index, (uint8_t)byte_val);
+}
+
+// 8: set-buffer
+static void
+ct_module_buffer_set_buffer(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, dst);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, index, as_uint);
+    CT_MODCALL_OBJECT_ARG(2, CtBufferObject*, src);
+
+    if (ct_lib_buffer_set_buffer(dst, index, src)) {
+        uint32_t written_upto = index + src->size;
+        CT_MODCALL_RESULT(as_uint, written_upto, CT_ATOM_PRIMITIVE);
+    }
+}
+
+// 9: push
+static void
+ct_module_buffer_push(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, byte_val, as_uint);
+
+    ct_lib_buffer_push_byte(buffer, (uint8_t)byte_val);
+}
+
+// 10: append
+static void
+ct_module_buffer_append(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, dst);
+    CT_MODCALL_OBJECT_ARG(1, CtBufferObject*, src);
+
+    ct_lib_buffer_append_buffer(dst, src);
+}
+
+// 11: slice
+static void
+ct_module_buffer_slice(CtModuleMethodArguments args, CtModuleMethodResult* result) {
+
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, index, as_uint);
+    CT_MODCALL_PRIMIT_ARG(2, uint32_t, length, as_uint);
+
+    CtBufferObject* sliced = ct_lib_buffer_slice(buffer, index, length);
+    if (sliced != NULL) {
+        CT_MODCALL_RESULT(as_object, (CtObject*)sliced, CT_ATOM_OBJECT);
+    }
+}
+
+// 12: copy
 static void
 ct_module_buffer_copy(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
 
-	CtBufferObject* copy;
-	result->success = copy = ct_lib_buffer_copy(buffer);
-
-	result->returned_atom.as_object = (CtObject*) copy;
-	result->returned_type = CT_ATOM_OBJECT;
+    CtBufferObject* copied = ct_lib_buffer_copy(buffer);
+    if (copied != NULL) {
+        CT_MODCALL_RESULT(as_object, (CtObject*)copied, CT_ATOM_OBJECT);
+    }
 }
 
-
-static void
-ct_module_buffer_get_byte(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint32_t index = args.argument_atoms[1].as_uint;
-
-	uint8_t byte;
-	result->success = ct_lib_buffer_get_byte(buffer, index, &byte);
-
-	result->returned_atom.as_uint = (uint32_t) byte;
-	result->returned_type = CT_ATOM_PRIMITIVE;
-}
-
-
-static void
-ct_module_buffer_set_byte(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint32_t index = args.argument_atoms[1].as_uint;
-	uint8_t byte = (uint8_t) args.argument_atoms[2].as_uint;
-
-	result->success = ct_lib_buffer_set_byte(buffer, index, byte);
-
-	result->returned_atom.as_uint = (uint32_t) byte;
-	result->returned_type = CT_ATOM_PRIMITIVE;
-}
-
-
-static void
-ct_module_buffer_set_buffer(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint32_t index = args.argument_atoms[1].as_uint;
-	CtBufferObject* other_buffer = (CtBufferObject*) args.argument_atoms[2].as_object;
-
-	result->success = ct_lib_buffer_set_buffer(buffer, index, other_buffer);
-
-	result->returned_atom.as_uint = index + other_buffer->size;
-	result->returned_type = CT_ATOM_PRIMITIVE;
-}
-
-
+// 13: fill
 static void
 ct_module_buffer_fill(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint8_t byte = args.argument_atoms[1].as_uint;
 
-	result->success = ct_lib_buffer_fill(buffer, byte);
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, byte_val, as_uint);
 
-	result->returned_atom.as_uint = (uint32_t) buffer->size;
-	result->returned_type = CT_ATOM_PRIMITIVE;
+    ct_lib_buffer_fill(buffer, (uint8_t)byte_val);
 }
 
+// 14: clear
 static void
 ct_module_buffer_clear(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
 
-	result->success = ct_lib_buffer_clear(buffer);
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
 
-	result->returned_atom.as_uint = (uint32_t) buffer->size;
-	result->returned_type = CT_ATOM_PRIMITIVE;
+    ct_lib_buffer_clear(buffer);
 }
 
+// 15: find-byte
 static void
-ct_module_buffer_extend(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	CtBufferObject* other_buffer = (CtBufferObject*) args.argument_atoms[1].as_object;
+ct_module_buffer_find_byte(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
-	result->success = ct_lib_buffer_extend(buffer, other_buffer);
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, start_idx, as_uint);
+    CT_MODCALL_PRIMIT_ARG(2, uint32_t, byte_val, as_uint);
 
-	result->returned_atom.as_uint = (uint32_t) buffer->size;
-	result->returned_type = CT_ATOM_PRIMITIVE;
+    uint32_t found_idx = UINT32_MAX;
+    if (ct_lib_buffer_find_byte(buffer, (uint8_t)byte_val, start_idx, &found_idx)) {
+        CT_MODCALL_RESULT(as_uint, found_idx, CT_ATOM_PRIMITIVE);
+    } else {
+        CT_MODCALL_RESULT(as_uint, UINT32_MAX, CT_ATOM_PRIMITIVE);
+    }
 }
 
-
+// 16: find-buffer
 static void
-ct_module_buffer_slice(CtModuleMethodArguments args, CtModuleMethodResult* result) {
-	
-	CtBufferObject* buffer = (CtBufferObject*) args.argument_atoms[0].as_object;
-	uint32_t index = args.argument_atoms[1].as_uint;
-	uint32_t length = args.argument_atoms[2].as_uint;
+ct_module_buffer_find_buffer(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
-	CtBufferObject* slice;
-	result->success = slice =ct_lib_buffer_slice(buffer, index, length);
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer);
+    CT_MODCALL_PRIMIT_ARG(1, uint32_t, start_idx, as_uint);
+    CT_MODCALL_OBJECT_ARG(2, CtBufferObject*, sub_buffer);
 
-	result->returned_atom.as_object = (CtObject*) slice;
-	result->returned_type = CT_ATOM_OBJECT;
+    uint32_t found_idx = UINT32_MAX;
+    if (ct_lib_buffer_find_buffer(buffer, sub_buffer, start_idx, &found_idx)) {
+        CT_MODCALL_RESULT(as_uint, found_idx, CT_ATOM_PRIMITIVE);
+    } else {
+        CT_MODCALL_RESULT(as_uint, UINT32_MAX, CT_ATOM_PRIMITIVE);
+    }
 }
 
+// 17: equals
+static void
+ct_module_buffer_equals(CtModuleMethodArguments args, CtModuleMethodResult* result) {
 
-CT_MODULE_NAME_DEFINE(buffer, "buffer");
+    CT_MODCALL_OBJECT_ARG(0, CtBufferObject*, buffer_a);
+    CT_MODCALL_OBJECT_ARG(1, CtBufferObject*, buffer_b);
+
+    bool is_equal = false;
+    if (ct_lib_buffer_equals(buffer_a, buffer_b, &is_equal)) {
+        CT_MODCALL_RESULT(as_bool, is_equal, CT_ATOM_PRIMITIVE);
+    }
+}
 
 CT_MODULE_METHOD_MAP_DEFINE(buffer) = {
-	CT_MODULE_ENTRY("new", ct_module_buffer_new, 1),
-	CT_MODULE_ENTRY("resize", ct_module_buffer_resize, 2),
-	CT_MODULE_ENTRY("get", ct_module_buffer_get_byte, 2),
-	CT_MODULE_ENTRY("set", ct_module_buffer_set_byte, 3),
-	CT_MODULE_ENTRY("setBuffer", ct_module_buffer_set_buffer, 3),
-	CT_MODULE_ENTRY("fill", ct_module_buffer_fill, 2),
-	CT_MODULE_ENTRY("clear", ct_module_buffer_clear, 1),
-	CT_MODULE_ENTRY("extend", ct_module_buffer_extend, 2),
-	CT_MODULE_ENTRY("slice", ct_module_buffer_slice, 3),
-	CT_MODULE_ENTRY("copy", ct_module_buffer_copy, 1),
+    [0]  = CT_MODULE_ENTRY("new",         ct_module_buffer_new,         1),
+    [1]  = CT_MODULE_ENTRY("size",        ct_module_buffer_size,        1),
+    [2]  = CT_MODULE_ENTRY("capacity",    ct_module_buffer_capacity,    1),
+    [3]  = CT_MODULE_ENTRY("resize",      ct_module_buffer_resize,      2),
+    [4]  = CT_MODULE_ENTRY("reserve",     ct_module_buffer_reserve,     2),
+    [5]  = CT_MODULE_ENTRY("truncate",    ct_module_buffer_truncate,    1),
+    [6]  = CT_MODULE_ENTRY("get",         ct_module_buffer_get,         2),
+    [7]  = CT_MODULE_ENTRY("set",         ct_module_buffer_set,         3),
+    [8]  = CT_MODULE_ENTRY("set-buffer",  ct_module_buffer_set_buffer,  3),
+    [9]  = CT_MODULE_ENTRY("push",        ct_module_buffer_push,        2),
+    [10] = CT_MODULE_ENTRY("append",      ct_module_buffer_append,      2),
+    [11] = CT_MODULE_ENTRY("slice",       ct_module_buffer_slice,       3),
+    [12] = CT_MODULE_ENTRY("copy",        ct_module_buffer_copy,        1),
+    [13] = CT_MODULE_ENTRY("fill",        ct_module_buffer_fill,        2),
+    [14] = CT_MODULE_ENTRY("clear",       ct_module_buffer_clear,       1),
+    [15] = CT_MODULE_ENTRY("find-byte",   ct_module_buffer_find_byte,   3),
+    [16] = CT_MODULE_ENTRY("find-buffer", ct_module_buffer_find_buffer, 3),
+    [17] = CT_MODULE_ENTRY("equals",      ct_module_buffer_equals,      2),
 };
-
-
 
 #endif // CT_MODULE_BUFFER_H
--- Engine/modules/modulespec.h
+++ Engine/modules/modulespec.h
@@ -18,8 +18,8 @@ typedef struct {
 typedef struct {
 	CtAtom         returned_atom;
 	CtAtomTypeSize returned_type;
-	bool           success;
 } CtModuleMethodResult;
+
 
 typedef void (*CtModuleMethod)(CtModuleMethodArguments, CtModuleMethodResult*);
 
@@ -29,10 +29,45 @@ typedef struct {
 	uint32_t       argument_count;
 } CtModuleMethodEntry;
 
+
 #define CT_MODULE_NAME_DEFINE(NAME, STRING) static const char* _ct_module_ ## NAME ## _name = STRING;
 #define CT_MODULE_METHOD_MAP_DEFINE(NAME) CtModuleMethodEntry _ct_module_ ## NAME ## _method_map[]
 #define CT_MODULE_METHOD_MAP(NAME) _ct_module_ ## NAME ## _method_map
 #define CT_MODULE_ENTRY(NAME, METHOD, ARG_COUNT) (CtModuleMethodEntry) {NAME, METHOD, ARG_COUNT}
 #define CT_MODULE_COUNT_METHODS(NAME) sizeof(CT_MODULE_METHOD_MAP(NAME)) / sizeof(CT_MODULE_METHOD_MAP(NAME)[0]) 
+
+
+// Short hand for getting a primitive. CtModuleMethodArguments must be args, and CtModuleMethodResult must be result. 
+#define CT_MODCALL_PRIMIT_ARG(INDEX, TYPE, NAME, ACCESSOR) \
+if (args.argument_types[INDEX] != CT_ATOM_PRIMITIVE) { \
+	CT_ERROR_ENGINE( \
+		ct_thread_error, \
+		"Module", \
+		"TypeMismatch", \
+		"Expected Primitive, got Object.", \
+		NULL \
+	); \
+	return; \
+}\
+TYPE NAME = (TYPE) (args.argument_atoms[INDEX].ACCESSOR); \
+
+// Short hand for getting an object. CtModuleMethodArguments must be args, and CtModuleMethodResult must be result. 
+#define CT_MODCALL_OBJECT_ARG(INDEX, TYPE, NAME) \
+if (args.argument_types[INDEX] != CT_ATOM_OBJECT) { \
+	CT_ERROR_ENGINE( \
+		ct_thread_error, \
+		"Module", \
+		"TypeMismatch", \
+		"Expected Object, got Primtive.", \
+		NULL \
+	); \
+	return; \
+}\
+TYPE NAME = (TYPE) (args.argument_atoms[INDEX].as_object); \
+
+
+#define CT_MODCALL_RESULT(ACCESSOR, VALUE, TYPE) \
+result->returned_atom.ACCESSOR = VALUE; \
+result->returned_type = TYPE; \
 
 #endif // ENGINE_MODULESPEC_H
